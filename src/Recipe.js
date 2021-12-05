39489ee164f61fc7d929e18740e0e88e858c9a95
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2021-12-05 15:42:41 -0500

    css
    wanted to improve the css.
    Also altered the recipe.js.
    Didn't like just listing the url it didn't look very neat.

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import style from './recipe.module.css';
+import React from "react";
+import style from "./recipe.module.css";
 
-
-const Recipe = ({title, image, ingredients, url}) => {
-    return(
+const Recipe = ({ title, image, ingredients, url }) => {
+  return (
     <div className={style.recipe}>
-        <h1>{title}</h1>
-        <p>You will need:</p>
-        <ol>
-            {ingredients.map(ingredient => (
-                <li>{ingredient.text}</li>
-            ))}
-        </ol>
-        <p>Want to make it tonight? Then visit:</p> 
-        <p><a href={url} target="_blank" rel="noreferrer">{url}</a></p>
-        <img className={style.image}src={image} alt=""/>
+      <h1>{title}</h1>
+      <p>You will need:</p>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+      <p>Want to make it tonight?</p>
+      <p>
+        <a href={url} target="_blank" rel="noreferrer">
+          Click Here
+        </a>
+      </p>
+      <img className={style.image} src={image} alt="" />
     </div>
-    );
-}
+  );
+};
 
 export default Recipe;
